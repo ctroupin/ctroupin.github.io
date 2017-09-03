c246f7c85fab1b4b1d884cb739fa0c60d634fdf0
ctroupin <charles.troupin@gmail.com>
2017-09-03 16:14:11 +0200

    GPX (500 points) for Mallorca

---
+++ leaflet/GPX/Mallorca/bikeList.js
@@ -0,0 +1,8 @@
+var bike = [
+"bike/palma-calvia-palmanova.gpx",
+"bike/palma-bunyola-orient-alaro-santa-maria-palma.gpx",
+"bike/palma-costa-calma.gpx",
+"bike/palma-santa-maria-del-cami-binissalem.gpx",
+"bike/palma-lloret-sencelles-santa-maria-palma.gpx",
+"bike/palma-son-veri-nou-palma.gpx",
+]

6a5f7ece5a3aec513101657325f032d74245bc75
ctroupin <charles.troupin@gmail.com>
2019-01-25 21:51:22 +0100

    rm leaflet

--- leaflet/data/ravitaillement.js
+++
@@ -1,6 +0,0 @@
-var ravitaillements = [
-	[50.58856, 5.84477, "Autoroute", '0 km'],
-	[50.57610, 5.84375, "Début Piedvache", '0 km'],
-	[50.57140, 5.85471, "Champs des Oiseaux", " "],
-	[50.57077, 5.87570, "Rue des Prés ", " "]
-];

4271466c9ab3c82b1bec1d4994d75b64f7a950f2
Troupin Charles <ctroupin@uliege.be>
2020-05-26 12:50:49 +0200

    updated CV

---
+++ _maps/maps/GPX/GranCanaria/ConfinementRunPhases.js
@@ -0,0 +1,101 @@
+var preconfinement = [
+"2020/grancanaria_2020_02_25_17_44_58.gpx",
+"2020/grancanaria_2020-02-26_16-20-36.gpx",
+"2020/grancanaria_2020-02-27_16-51-57.gpx",
+"2020/grancanaria_2020-02-28_18-03-27.gpx",
+"2020/grancanaria_2020-02-29_08-15-30.gpx",
+"2020/grancanaria_2020-02-29_10-54-54.gpx",
+"2020/grancanaria_2020-03-01_11-42-22.gpx",
+"2020/grancanaria_2020-03-02_14-35-30.gpx",
+"2020/grancanaria_2020-03-02_17-05-09.gpx",
+"2020/grancanaria_2020-03-03_16-56-42.gpx",
+"2020/grancanaria_2020-03-04_09-27-05.gpx",
+"2020/grancanaria_2020-03-04_17-05-52.gpx",
+"2020/grancanaria_2020-03-05_17-24-45.gpx",
+"2020/grancanaria_2020-03-06_13-52-24.gpx",
+"2020/grancanaria_2020-03-07_16-44-52.gpx",
+"2020/grancanaria_2020-03-08_13-43-55.gpx",
+"2020/grancanaria_2020-03-08_18-49-52.gpx",
+"2020/grancanaria_2020-03-09_19-54-21.gpx",
+"2020/grancanaria_2020-03-10_16-33-58.gpx",
+"2020/grancanaria_2020-03-11_16-45-20.gpx",
+"2020/grancanaria_2020-03-12_04-58-33.gpx",
+"2020/grancanaria_2020-03-12_13-30-10.gpx",
+"2020/grancanaria_2020-03-13_16-23-14.gpx",
+"2020/grancanaria_2020-03-14_17-20-05.gpx",
+]
+
+var confinement = [
+"2020/Confinement/grancanaria_2020-03-24_15-45-21.gpx",
+"2020/Confinement/grancanaria_2020-04-05_11-51-34.gpx",
+"2020/Confinement/grancanaria_2020-04-07_11-09-22.gpx",
+"2020/Confinement/grancanaria_2020-04-08_18-37-43.gpx",
+"2020/Confinement/grancanaria_2020-04-09_10-05-59.gpx",
+"2020/Confinement/grancanaria_2020-04-10_10-41-31.gpx",
+"2020/Confinement/grancanaria_2020-04-11_18-44-47.gpx",
+"2020/Confinement/grancanaria_2020-04-12_12-22-23.gpx",
+"2020/Confinement/grancanaria_2020-04-13_18-04-59.gpx",
+"2020/Confinement/grancanaria_2020-04-14_19-28-28.gpx",
+"2020/Confinement/grancanaria_2020-04-15_19-11-13.gpx",
+"2020/Confinement/grancanaria_2020-04-16_18-47-26.gpx",
+"2020/Confinement/grancanaria_2020-04-17_11-38-09.gpx",
+"2020/Confinement/grancanaria_2020-04-18_19-04-30.gpx",
+"2020/Confinement/grancanaria_2020-04-19_19-05-55.gpx",
+"2020/Confinement/grancanaria_2020-04-20_11-41-56.gpx",
+"2020/Confinement/grancanaria_2020-04-21_11-36-25.gpx",
+"2020/Confinement/grancanaria_2020-04-22_19-34-21.gpx",
+"2020/Confinement/grancanaria_2020-04-23_10-59-22.gpx",
+"2020/Confinement/grancanaria_2020-04-24_18-33-44.gpx",
+"2020/Confinement/grancanaria_2020-04-25_18-52-00.gpx",
+"2020/Confinement/grancanaria_2020-04-26_12-07-54.gpx",
+"2020/Confinement/grancanaria_2020-04-27_18-42-20.gpx",
+]
+
+var movePhase0 = [
+"2020/Phase0/grancanaria_2020_05_02_12_18_32.gpx",
+"2020/Phase0/grancanaria_2020_05_03_11_43_37.gpx",
+"2020/Phase0/grancanaria_2020_05_04_11_47_23_Running.gpx",
+"2020/Phase0/grancanaria_2020_05_05_12_10_05_Running.gpx",
+"2020/Phase0/grancanaria_2020_05_06_12_41_07_Running.gpx",
+"2020/Phase0/grancanaria_2020_05_07_12_02_10_Running.gpx",
+"2020/Phase0/grancanaria_2020_05_08_09_23_34_Running.gpx",
+"2020/Phase0/grancanaria_2020_05_09_16_33_30_Running.gpx",
+"2020/Phase0/grancanaria_2020_05_10_13_09_48_Running.gpx"
+]
+
+var movePhase1 = [
+"2020/Phase1/grancanaria_2020_05_11_12_48_58_Running.gpx",
+"2020/Phase1/grancanaria_2020_05_12_18_08_31_Running.gpx",
+"2020/Phase1/grancanaria_2020_05_13_18_34_59_Running.gpx",
+"2020/Phase1/grancanaria_2020_05_14_18_44_22_Running.gpx",
+"2020/Phase1/grancanaria_2020_05_15_18_39_34_Running.gpx",
+"2020/Phase1/grancanaria_2020_05_16_12_37_21_Running.gpx",
+"2020/Phase1/grancanaria_2020_05_17_16_36_01_Running.gpx",
+"2020/Phase1/grancanaria_2020-05-18_17-31-39.gpx",
+"2020/Phase1/grancanaria_2020-05-19_09-51-16.gpx",
+"2020/Phase1/grancanaria_2020-05-20_17-56-34.gpx",
+"2020/Phase1/grancanaria_2020-05-21_09-33-40.gpx",
+"2020/Phase1/grancanaria_2020-05-22_15-57-30.gpx",
+"2020/Phase1/grancanaria_2020-05-23_16-50-25.gpx",
+"2020/Phase1/grancanaria_2020-05-24_17-15-17.gpx",
+]
+
+var movePhase2 = [
+"2020/Phase2/grancanaria_2020_05_25_18_27_41_Running.gpx"
+]
+
+
+var hiking = [
+  "2020/Hiking/grancanaria_2020-04-26_13-18-00.gpx",
+
+  "2020/Hiking/grancanaria_2020-04-27_15-03-38.gpx",
+  "2020/Hiking/grancanaria_2020_04_30_17_40_49.gpx",
+  "2020/Hiking/grancanaria_2020_05_03_08_32_43.gpx",
+  "2020/Hiking/grancanaria_2020-05-06_16-09-00.gpx",
+  "2020/Hiking/grancanaria_2020_05_08_15_43_54_Trekking.gpx",
+  "2020/Hiking/grancanaria_2020_05_09_09_02_09_Trekking.gpx",
+  "2020/Hiking/grancanaria_2020_05_11_16_52_06_Trekking.gpx",
+  "2020/Hiking/grancanaria_2020_05_12_15_37_53_Trekking.gpx",
+  "2020/Hiking/grancanaria_2020_05_15_14_17_30_Trekking.gpx",
+  "2020/Hiking/grancanaria_2020-05-19_14-15-18.gpx",
+]
